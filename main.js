b521dd6adb9630529bf68f314540d5de8abf22ea
keanii2018 <keani.mpcc@outlook.com>
2022-07-11 11:59:01 -0700

    home section styling

--- main.js
+++ main.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing effects
     var typed = new Typed(".typing", {
-        strings:["Father", "Software Engineer", "Small Business Owner", "Writer", "Team Player", "COMMUNIST" ],
+        strings:["Father", "Software Engineer", "Small Business Owner", "Writer", "Team Player"],
         typeSpeed: 100,
         backSpeed:60,
         loop:true
     });
     
     var typed = new Typed(".typing-2", {
-        strings: ["Father", "Software Engineer", "Small Business Owner", "Writer", "Team Player", "COMMUNIST" ],
+        strings: ["Father", "Software Engineer", "Small Business Owner", "Writer", "Team Player", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
